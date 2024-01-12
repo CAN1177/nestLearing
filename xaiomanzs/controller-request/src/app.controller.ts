1add56e0c0d88cf185e6fe66890848e752561382
Silva <39960526+CAN1177@users.noreply.github.com>
2024-01-12 14:53:26 +0800

    feat: Providers入门

--- xaiomanzs/controller-request/src/app.controller.ts
+++ xaiomanzs/controller-request/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('ABC') private readonly appService: AppService,
+    @Inject('Def') private readonly show: string[],
+    @Inject('CCC') private readonly ccc: string,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.ccc;
   }
 }
--- xaiomanzs/controller-request/src/app.module.ts
+++ xaiomanzs/controller-request/src/app.module.ts
@@ -1,11 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Module, Inject } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppService2 } from './app.service2';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService2,
+    {
+      provide: 'ABC',
+      useClass: AppService,
+    },
+    {
+      provide: 'Def',
+      useValue: ['Hello World 自定义注入值', '哈哈哈'],
+    },
+    {
+      provide: 'CCC',
+      inject: [AppService2],
+      // useFactory: (AppService2: AppService2) => {
+      //   console.log(
+      //     '%c Line:24 🍖 AppService2',
+      //     'color:#ed9ec7',
+      //     AppService2.getHello(),
+      //   );
+      //   return '123 AppService2';
+      // },
+      // 异步工厂模式
+      useFactory: async (AppService2: AppService2) => {
+        return await new Promise((r) => {
+          setTimeout(() => {
+            r(AppService2.getHello());
+          }, 2000);
+        });
+      },
+    },
+  ],
 })
 export class AppModule {}
